c15a6688c2dfcf427629beb0c7be680490bd8c33
Madduru_Nikhil <nmadduru@uworld.in>
2023-12-18 14:24:01 +0530

    commit UI Changes

--- student-app/src/app/context-container/context-container.component.ts
+++ student-app/src/app/context-container/context-container.component.ts
@@ -34,7 +34,7 @@ export class ContextContainerComponent implements OnInit {
   }
 
   get disableGenerateQuestions(): boolean {
-    return !this.contextForm || this.contextForm.length < 1000 || this.generatingQuestions;
+    return !this.contextForm || this.contextForm.length < 500 || this.generatingQuestions;
   }
 
   get fetchSubmitBtnText(): string {
@@ -48,6 +48,14 @@ export class ContextContainerComponent implements OnInit {
     const file: File = event.target.files[0];
     if (file) {
       this.file = file;
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const filecontent = e.target.result;
+        console.log('File Conent: ', filecontent);
+
+        this.contextForm = filecontent;
+      }
+      reader.readAsText(file);
     } else {
       console.error('No file selected.');
     }
